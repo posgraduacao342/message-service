8d786a8f5433e0c92993d281f3898b1fb5d85486
Matheus Fernando Golmini <matheusfernandogolmini@MacBook-Air-de-Matheus.local>
2024-03-02 09:04:51 -0300

    add log

--- src/application/consumers/EmailConsumer.ts
+++ src/application/consumers/EmailConsumer.ts
@@ -18,6 +18,7 @@ export class EmailConsumer {
     routingKey: 'enviar.email',
   })
   async consume(mensagem: MensagemDto) {
+    console.log('Mensagem', mensagem);
     await this.servicoEmail.enviarEmail(mensagem);
   }
 }
